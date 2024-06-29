039b357f3b7f681148fb1deff6536de01551e8f6
Qzimyion <samyakchauhan676@gmail.com>
2024-06-29 18:02:14 +0530

    updated cache

--- common/src/main/java/com/qzimyion/blocks/DecayingCloudBlock.java
+++ common/src/main/java/com/qzimyion/blocks/DecayingCloudBlock.java
@@ -1,5 +1,6 @@
 package com.qzimyion.blocks;
 
+import com.qzimyion.registry.BlockRegistry;
 import com.qzimyion.registry.ItemRegistry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -11,11 +12,16 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BucketPickup;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.material.FluidState;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -23,13 +29,23 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Optional;
 
 public class DecayingCloudBlock extends NonNewtonianBlock implements BucketPickup {
+    public static final int STABILITY_MAX_DISTANCE = 7;
+    public static final IntegerProperty DISTANCE = BlockStateProperties.STABILITY_DISTANCE;
 
     public DecayingCloudBlock(Properties properties) {
         super(properties);
+        this.registerDefaultState(this.stateDefinition.any().setValue(DISTANCE, 0));
     }
 
     @Override
     protected void tick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
+        int i = getDistance(serverLevel, blockPos);
+        BlockState state = blockState.setValue(DISTANCE, i);
+        if (blockState.getValue(DISTANCE) == STABILITY_MAX_DISTANCE) {
+            serverLevel.destroyBlock(blockPos, true);
+        } else if (state != blockState) {
+            serverLevel.setBlock(blockPos, blockState, 3);
+        }
         if (this.CloudDecay(serverLevel, blockPos)) {
             serverLevel.setBlock(blockPos, VaporisesInto(), 2);
         }
@@ -49,12 +65,15 @@ public class DecayingCloudBlock extends NonNewtonianBlock implements BucketPicku
 
     @Override
     @Nullable
-    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
-        if (this.CloudDecay(blockPlaceContext.getLevel(), blockPlaceContext.getClickedPos())) {
-            blockPlaceContext.getLevel().scheduleTick(blockPlaceContext.getClickedPos(), this,
-                    100 + blockPlaceContext.getLevel().getRandom().nextInt(80));
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        BlockPos blockPos = context.getClickedPos();
+        Level level = context.getLevel();
+        int i = getDistance(level, blockPos);
+        if (this.CloudDecay(context.getLevel(), context.getClickedPos())) {
+            context.getLevel().scheduleTick(context.getClickedPos(), this,
+                    100 + context.getLevel().getRandom().nextInt(80));
         }
-        return this.defaultBlockState();
+        return this.defaultBlockState().setValue(DISTANCE, i);
     }
 
     protected boolean CloudDecay(BlockGetter blockGetter, BlockPos blockPos) {
@@ -64,6 +83,33 @@ public class DecayingCloudBlock extends NonNewtonianBlock implements BucketPicku
             return false;
         }
         return true;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        builder.add(DISTANCE);
+    }
+
+    @Override
+    public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        return getDistance(level, pos) < STABILITY_MAX_DISTANCE;
+    }
+
+
+    public static int getDistance(BlockGetter level, BlockPos pos) {
+        BlockPos.MutableBlockPos mutableBlockPos = pos.mutable().move(Direction.DOWN);
+        int i = STABILITY_MAX_DISTANCE;
+        for (Direction direction : Direction.Plane.HORIZONTAL) {
+            BlockState blockState2 = level.getBlockState(mutableBlockPos.setWithOffset(pos, direction));
+            if (blockState2.is(BlockRegistry.CLOUD_BLOCK.get())) {
+                i = Math.min(i, blockState2.getValue(DISTANCE) + 1);
+                if (i == 1) {
+                    break;
+                }
+            }
+        }
+
+        return i;
     }
 
     @Override
